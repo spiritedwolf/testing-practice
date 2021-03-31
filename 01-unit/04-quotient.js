41c4422a67a9fc9ec0f9f313075105a8a7f36ef8
Spiritedwolf <sufismoker31@gmail.com>
2021-03-31 22:03:34 +0530

    added quotient functionality in 04-quotient.js file

--- 01-unit/04-quotient.js
+++ 01-unit/04-quotient.js
@@ -1,8 +1,12 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function quotient() {
-
+function quotient(num1 = 0, num2 = 1) {
+  if (num2 ==0) {
+    return "Error";
+  }
+  total = num1 / num2;
+  return total
 }
 
 // ==================================================
@@ -20,21 +24,29 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return the text 'ERROR' when the second number is 0.
+  var result = quotient(2, 0);
+  if (result !== "Error") throw new Error('Expected quotient(2, 0) to be "Error". Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = quotient(2, 2, 2);
+  if (result !== 1) throw new Error('Expected quotient(2, 2, 2) to be 1. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = quotient(2);
+  if (result !== 2) throw new Error('Expected quotient(2) to be 2. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = quotient();
+  if (result !== 0) throw new Error('Expected quotient() to be 0. Received: ' + result);
 
   console.log('All tests passed successfully.');
 
