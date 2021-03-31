5dc028d03c2fcd8db1e0445312a826ecdcf5fad4
Spiritedwolf <sufismoker31@gmail.com>
2021-03-31 21:50:02 +0530

    added sum functionality in 01-sum.js file

--- 01-unit/01-sum.js
+++ 01-unit/01-sum.js
@@ -1,9 +1,11 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function sum() {
-
+function sum(num1 = 0, num2 = 0) {
+  total = num1 + num2;
+  return total;
 }
+  
 
 // ==================================================
 // DEFINE AND EXECUTE TEST CASES
@@ -20,16 +22,22 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = sum(1, 1, 1);
+  if (result !== 2) throw new Error('Expected sum(1, 1, 1) to be 2. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = sum(1);
+  if (result !== 1) throw new Error('Expected sum(1) to be 1. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = sum();
+  if (result !== 0) throw new Error('Expected sum() to be 0. Received: ' + result);
 
   console.log('All tests passed successfully.');
 
