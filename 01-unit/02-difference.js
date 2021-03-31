60da5d36ca27f7283f271996b9d842a699930b16
Spiritedwolf <sufismoker31@gmail.com>
2021-03-31 21:54:30 +0530

    added difference functionality in 02-difference.js file

--- 01-unit/02-difference.js
+++ 01-unit/02-difference.js
@@ -1,8 +1,9 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function difference() {
-
+function difference(num1 = 0, num2 = 0) {
+  total = num1 - num2;
+  return total;
 }
 
 // ==================================================
@@ -20,16 +21,22 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = difference(1, 1, 1);
+  if (result !== 0) throw new Error('Expected difference(1, 1, 1) to be 0. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = difference(1);
+  if (result !== 1) throw new Error('Expected difference(1) to be 1. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = difference();
+  if (result !== 0) throw new Error('Expected difference() to be 0. Received: ' + result);
 
   console.log('All tests passed successfully.');
 
