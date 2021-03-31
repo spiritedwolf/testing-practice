cbc9a6aa0fcc91620679485a677562d2ac760ae5
Spiritedwolf <sufismoker31@gmail.com>
2021-03-31 21:58:27 +0530

    added multiplication functionality in 03-product.js file

--- 01-unit/03-product.js
+++ 01-unit/03-product.js
@@ -1,8 +1,9 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function product() {
-
+function product(num1 = 0, num2 = 1) {
+  total = num1 * num2;
+  return total;
 }
 
 // ==================================================
@@ -20,21 +21,29 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return 0 when either of the numbers are 0.
+  var result = product(2, 0);
+  if (result !== 0) throw new Error('Expected product(2, 0) to be 0. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = product(2, 2, 2);
+  if (result !== 4) throw new Error('Expected product(2, 2, 2) to be 4. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = product(2);
+  if (result !== 2) throw new Error('Expected product(2) to be 2. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = product();
+  if (result !== 0) throw new Error('Expected product() to be 0. Received: ' + result);
 
   console.log('All tests passed successfully.');
 
